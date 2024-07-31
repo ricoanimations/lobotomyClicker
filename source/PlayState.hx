035102b78d227a43ee17e3ccf7504f5d7d751b09
Rico Webb <119141397+ricoanimations@users.noreply.github.com>
2024-07-31 16:36:46 -0400

    a fresh and minty clean build

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,8 +1,10 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 
 class PlayState extends FlxState
@@ -11,6 +13,7 @@ class PlayState extends FlxState
 	var face:FlxSprite;
 	var lobotomies:FlxText;
 	var number:Int = 0;
+
 	override public function create()
 	{
 		super.create();
@@ -21,8 +24,9 @@ class PlayState extends FlxState
 		face = new FlxSprite();
 		face.loadGraphic("assets/images/unrated.png");
 		face.scale.set(0.25, 0.25);
-		face.x = 0;
-		face.y = -500;
+		face.updateHitbox();
+		face.x = 500;
+		face.y = 500;
 		add(face);
 
 		lobotomies = new FlxText(0, 100, 0, '${number} lobotomies', 48);
@@ -33,5 +37,23 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		if (FlxG.mouse.overlaps(face))
+		{
+			if (FlxG.mouse.pressed)
+			{
+				face.scale.set(0.275, 0.275);
+				face.updateHitbox();
+				number += 1;
+				remove(lobotomies);
+				lobotomies = new FlxText(0, 100, 0, '${number} lobotomies', 48);
+				lobotomies.setFormat("Times New Roman", 48);
+				add(lobotomies);
+			}
+			if (FlxG.mouse.justReleased)
+			{
+				face.scale.set(0.25, 0.25);
+				face.updateHitbox();
+			}
+		}
 	}
 }
