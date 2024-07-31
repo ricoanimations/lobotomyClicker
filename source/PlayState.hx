49a3a63b93b6cf657931cf2e965a2ea38111fde3
Rico Webb <119141397+ricoanimations@users.noreply.github.com>
2024-07-31 14:36:57 -0400

    stuff

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,33 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
+	var bg:FlxSprite;
+	var face:FlxSprite;
+	var lobotomies:FlxText;
+	var number:Int = 0;
 	override public function create()
 	{
 		super.create();
+		bg = new FlxSprite();
+		bg.makeGraphic(1280, 720, FlxColor.GRAY);
+		add(bg);
+
+		face = new FlxSprite();
+		face.loadGraphic("assets/images/unrated.png");
+		face.scale.set(0.25, 0.25);
+		face.x = 0;
+		face.y = -500;
+		add(face);
+
+		lobotomies = new FlxText(0, 100, 0, '${number} lobotomies', 48);
+		lobotomies.setFormat("Times New Roman", 48);
+		add(lobotomies);
 	}
 
 	override public function update(elapsed:Float)
