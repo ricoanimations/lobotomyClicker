9304f42b3c8534dbf7d5779c1acc290981a3773c
Rico Webb <119141397+ricoanimations@users.noreply.github.com>
2024-09-03 16:33:22 -0400

    the most useless commit ever

--- source/Main.hx
+++ source/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxGame;
 import openfl.display.FPS;
 import openfl.display.Sprite;
@@ -13,5 +14,9 @@ class Main extends Sprite
 		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
 		#end
+		if (FlxG.keys.pressed.CONTROL && FlxG.keys.justPressed.F)
+		{
+			FlxG.fullscreen = !FlxG.fullscreen;
+		}
 	}
 }
